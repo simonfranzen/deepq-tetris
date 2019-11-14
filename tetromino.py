1a56c1635a22c6ee55927dc0fae23f791abde386
koch_p <koch@prime-research.com>
2019-11-15 00:54:54 +0100

    renamed tetrominos to tetromino, since each instance holds one of them at a time. added function to rotate tetromino in both directions

---
+++ tetromino.py
@@ -0,0 +1,34 @@
+import random
+import numpy as np
+
+class Tetromino:
+
+    def __init__(self):
+        # initialize possible shapes
+        self.i = np.array([[0,1,0,0], [0,1,0,0], [0,1,0,0], [0,1,0,0]], int)
+        self.o = np.array([[1,1],[1,1]], int)
+        self.z = np.array([[1,1,0],[0,1,0],[0,1,1]], int)
+        self.l = np.array([[0,1,0],[0,1,0],[0,1,1]], int)
+        self.tetrominos = np.array([self.i, self.o, self.z, self.l])
+        # select random tetromino
+        self.tetromino = self.tetrominos[(random.randint(0, len(self.tetrominos)-1))]
+
+    def rotate(self, counter_clockwise = True):
+        """ rotates a tetromino by 90 degrees """
+        steps = 1
+        if(counter_clockwise == False):
+            steps = 3
+        self.tetromino = np.rot90(self.tetromino, steps)
+
+    def get(self):
+        return self.tetromino
+
+
+#tetromino = Tetromino()
+#print(tetromino.get())
+#tetromino.rotate(False)
+#print(tetromino.get())
+#tetromino.rotate(False)
+#print(tetromino.get())
+#tetromino.rotate(True)
+#print(tetromino.get())
