eea6a162023493d845a0ba5700be8adf44fb244a
Simon Franzen <simon@zauberware.com>
2019-11-15 02:46:45 +0100

    reach key with keyboard.Listener

--- run.py
+++ run.py
@@ -1,42 +1,65 @@
 import threading
 import queue
 import time
+import os
+from getch import _Getch
 
-def read_kbd_input(inputQueue):
-    print('Ready for keyboard input:')
-    while (True):
-        key = input()
-        inputQueue.put(key)
+from pynput import keyboard
 
-def run():
-    EXIT_COMMAND = 'exit'
-    inputQueue = queue.Queue()
+# from tetris_environment import TetrisEnvironment
 
-    inputThread = threading.Thread(target=read_kbd_input, args=(inputQueue,), daemon=True)
-    inputThread.start()
 
-    while (True):
-        if (inputQueue.qsize() > 0):
-            key = inputQueue.get()
-            print('key = {}'.format(key))
+input_queue = queue.Queue()
 
-            if (key == EXIT_COMMAND):
-                print('Exiting serial terminal.')
+def on_press(key):
+        try:
+            print(key)
+            input_queue.put(key)
+            print(input_queue.qsize())
+        except:
+            print('Error wit key {0}.'.format(key))
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+
+# def read_kbd_input(input_queue):
+#     while True:
+#         getch = _Getch()
+#         input_queue.put(getch.impl.getch())
+
+
+
+def main():
+
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+    print("after keyboard listener")
+
+    while True:
+        if input_queue.qsize() > 0:
+            key = input_queue.get()
+            print("Game Loop")
+            print(key)
+            if key == 'q':
+                print('Exit GAME')
+                is_running = False
                 break
-            elif key == 260:
+            elif key == 'a':
                 print('move left')
-            elif key == 261:
+            elif key == 'd':
                 print('move right')
-            elif key == 258:
+            elif key == 's':
                 print('move down')
             else:
-                continue
+                print('wait')
 
-        time.sleep(1)
-        print('hello')
+        time.sleep(0.1)
+        print('print game')
 
-    print('End.')
 
 
 if __name__ == '__main__':
-    run()
+    main()
